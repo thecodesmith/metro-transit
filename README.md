67b6f2a476b3e67246b30284837762fba92a190c
Brian Stewart <brian.stewart@jamf.com>
2024-01-23 11:49:31 -0600

    Update to use Metro Transit API v2

--- README.md
+++ README.md
@@ -6,5 +6,14 @@ This script pulls the current bus locations from the Metro Transit NexTrip API,
 and prints a URL to https://geojson.io with the points plotted. Very
 rudimentary at the moment.
 
+Usage:
+
     npm install
-    node wheres-my-bus.js [route] | xargs open
+    ./wheres-my-bus.js [route] | xargs open
+
+Example:
+
+    ./wheres-my-bus.js 14 | xargs open
+
+The Metro Transit NexTrip API is documented here:
+- https://svc.metrotransit.org/swagger/index.html
--- wheres-my-bus.js
+++ wheres-my-bus.js
@@ -3,19 +3,30 @@
 const request = require('request');
 const _ = require('lodash');
 
-const route = process.argv[2] || '535';
-const api = `http://svc.metrotransit.org/NexTrip/VehicleLocations/${route}`;
-const url = 'http://geojson.io/#data=data:application/json,'
+const route = process.argv[2] || '14';
+const api = `https://svc.metrotransit.org/nextrip/vehicles/${route}`;
+const url = 'https://geojson.io/#data=data:application/json,'
 
 request(api, { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  if (res.statusCode != 200) {
+    console.log(`Error [${res.statusCode}]: ${body.title} - ${body.detail}`);
+    process.exit(1);
+  }
+
   let geojsonData = generateGeoJson(body);
   console.log(url + toUrl(geojsonData));
 });
 
 let generateGeoJson = function(data) {
-  let features = data.map(function(entry) {
-    return point(entry.VehicleLatitude, entry.VehicleLongitude);
+  let features = data.filter(function(entry) {
+    return entry.latitude != 0 && entry.longitude != 0;
+  }).map(function(entry) {
+    return point(entry.latitude, entry.longitude);
   });
 
   return {
